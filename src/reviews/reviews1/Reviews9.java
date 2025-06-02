10ba4d26a1016a8a7b0172b92a0aaa2fb4dcb9bf
241026 <241026@std.hi-joho.ac.jp>
2025-06-02 12:10:27 +0900

    デモの作成

--- src/reviews/reviews1/Reviews9.java
+++ src/reviews/reviews1/Reviews9.java
@@ -1,4 +1,16 @@
 package reviews.reviews1;
-
+import java.io.*;
 public class Reviews9 {
+    public static void main(String[] args)throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        System.out.print("名前を入力してください。");
+        String inputName = br.readLine();
+        System.out.print("年齢を入力してください。");
+        int age = Integer.parseInt(br.readLine());
+        System.out.print(age + "歳の" + inputName + "さん。\n こんにちは。");
+
+
+
+    }
 }
